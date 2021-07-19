7442be1d7ad1217366ccd3d4331816e13217c7af
Kevin Oliveira <kevin.zero@hotmail.com>
2021-07-18 21:30:38 -0300

    Updates

---
+++ precache-manifest.6af940e74173461bd13c9ad687fa2839.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de47f41d5eefdcfedc47c964412e8c1",
+    "url": "/project-movie-card-library-crud/index.html"
+  },
+  {
+    "revision": "75fedf90b387182e293e",
+    "url": "/project-movie-card-library-crud/static/css/main.c2aa0e3b.chunk.css"
+  },
+  {
+    "revision": "f2b89541f1c561a6ec66",
+    "url": "/project-movie-card-library-crud/static/js/2.d5b73cc1.chunk.js"
+  },
+  {
+    "revision": "75fedf90b387182e293e",
+    "url": "/project-movie-card-library-crud/static/js/main.7b6212b4.chunk.js"
+  },
+  {
+    "revision": "c40cdb837737929da9f3",
+    "url": "/project-movie-card-library-crud/static/js/runtime-main.41deedbf.js"
+  }
+]);
